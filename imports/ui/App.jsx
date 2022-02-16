1e5d68a8cc60ad0f08acc23f22c95dd7cbd2df09
krystofsvaton <svatykr@gmail.com>
2022-02-16 21:34:03 +0100

    create printer app, files structure

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import PrinterApp from './components/printer-app';
+import styled from "styled-components";
 
 export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-  </div>
+    <AppWrapper>
+        <PrinterApp/>
+    </AppWrapper>
 );
+
+const AppWrapper = styled.div``
