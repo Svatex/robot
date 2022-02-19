026784da41672d51bb27706db649571844a106fc
krystofsvaton <svatykr@gmail.com>
2022-02-19 18:17:42 +0100

    labels, styling

--- imports/ui/components/printer-app.jsx
+++ imports/ui/components/printer-app.jsx
@@ -4,14 +4,14 @@ import {Form, Formik, Field} from 'formik';
 import * as Yup from "yup";
 
 const validationSchema = Yup.object().shape({
-    startNum:  Yup
+    startNum: Yup
         .number("Must be a number")
-        .integer("Must be and integr")
-        .min(0, "Must be bigger than 0")
+        .integer("Must be and integer")
+        .min(1, "Must be bigger than 0")
         .required("Required"),
     endNum: Yup
         .number("Must be a number")
-        .integer("Must be and integr")
+        .integer("Must be and integer")
         .min(1, "Must be bigger than 1")
         .required("Required"),
 });
@@ -24,8 +24,7 @@ const PrinterApp = () => {
     const numberArray = Array.from({length: endNum - startNum}, (_x, i) => i + startNum)
 
     const handleSubmit = (values) => {
-        console.log(values)
-        setEndNum(values.endNum)
+        setEndNum(values.endNum + 1)
         setStartNum(values.startNum)
     }
 
@@ -41,33 +40,41 @@ const PrinterApp = () => {
                     handleSubmit(values)
                 }}
             >
-                {({ errors, touched }) => (
-                    <Form>
+                {({errors, touched}) => (
+                    <CustomForm>
+                        <label htmlFor="startNum">Start Number</label>
                         <Field
+                            id="startNum"
                             name="startNum"
                             type="number"
+                            placeholder="First number"
                         />
                         {errors.startNum && touched.startNum
                             && <ErrorText>{errors.startNum}</ErrorText>
                         }
+                        <label htmlFor="endNum">End Number</label>
                         <Field
+                            id="endNum"
                             name="endNum"
                             type="number"
+                            placeholder="Last number"
+
                         />
                         {errors.endNum && touched.endNum
                             && <div>{errors.endNum}</div>
                         }
                         <button type="submit">Submit</button>
-                    </Form>
+                    </CustomForm>
                 )}
             </Formik>
-
-            {numberArray.map((num) => {
-                if (num % 5 === 0 && num % 3 === 0) return "robotICT "
-                if (num % 3 === 0) return "Robot "
-                if (num % 5 === 0) return "ICT "
-                return num + " "
-            })}
+            <NumbersAndRobots>
+                {numberArray.map((num) => {
+                    if (num % 5 === 0 && num % 3 === 0) return <PrintedField>robotICT</PrintedField>
+                    if (num % 3 === 0) return <PrintedField>Robot</PrintedField>
+                    if (num % 5 === 0) return <PrintedField>ICT</PrintedField>
+                    return <PrintedField>{num}</PrintedField>
+                })}
+            </NumbersAndRobots>
         </PrinterWrapper>
     )
 
@@ -76,9 +83,35 @@ export default PrinterApp
 
 const PrinterWrapper = styled.div`
   max-width: 1280px;
-  background-color: gray;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 
 const ErrorText = styled.div`
-    color: red;
+  font-size: 9px;
+  color: red;
+`
+
+const CustomForm = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  width: 30%;
+  margin: 50px auto 20px auto;
+`
+
+const NumbersAndRobots = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+`
+
+const PrintedField = styled.p`
+  width: 75px;
+  text-align: center;
+  border: 1px black solid;
+  margin: 5px;
 `
